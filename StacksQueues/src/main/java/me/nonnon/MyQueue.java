66e418ba7eede2dc6cc686e33aac6b1cee158234
Ha Hwang <spyderthing@gmail.com>
2020-06-20 20:24:19 -0400

    Added methods for MyQueue class

--- StacksQueues/src/main/java/me/nonnon/MyQueue.java
+++ StacksQueues/src/main/java/me/nonnon/MyQueue.java
@@ -1,5 +1,49 @@
 package me.nonnon;
 
-public class MyQueue {
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+	private static class QueueNode<T> {
+		private T data;
+		private QueueNode<T> next;
+		
+		public QueueNode(T data) {
+			this.data = data;			
+		}
+	}
+	
+	private QueueNode<T> first;
+	private QueueNode<T> last;
+	
+	public void add(T item) {
+		QueueNode<T> n = new QueueNode<T>(item);
+		if (last != null) {
+			last.next = n;			
+		}
+		last = n;
+		if (first == null) {
+			first = last;
+		}		
+	}
+	
+	public T removeNode() {
+		if (first == null) throw new NoSuchElementException();
+		T data = first.data;
+		first = first.next;
+		if (first == null) {
+			last = null;
+		}
+		return data;
+		
+	}
+	
+	public T peek() {
+		if (first == null) throw new NoSuchElementException();
+		return first.data;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
 
 }
